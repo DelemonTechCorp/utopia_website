2bf45edef864e7e525af0f40611ab0810fa31e5a
aparna <ekaparna3@gmail.com>
2026-05-14 12:24:39 +0530

    updates

--- utopia_realty/settings.py
+++ utopia_realty/settings.py
@@ -22,19 +22,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure--49-#=at@a1t1+&5o(ik$2yh3b(la0%x#=4pk$_qxruana#1*v'
 
-# # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
 # ALLOWED_HOSTS = []
+
+# ----------------------------------------------------------- #
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+
 # Application definition
 
 INSTALLED_APPS = [
